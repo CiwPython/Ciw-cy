836feb02a6625ce42f705f1cdd47d5cc634edbcc
Geraint Palmer <palmer.geraint@googlemail.com>
2016-06-22 11:23:17 +0100

    bottwm 'Ewch' yn fwy, css override

--- docs/conf.py
+++ docs/conf.py
@@ -157,6 +157,7 @@ html_favicon = '_static/favicon.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
@@ -300,3 +301,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+def setup(app):
+   app.add_stylesheet("style.css")
