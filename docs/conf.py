063fbc6dbbf0ce19811d5cfbfd389b0981b74d9c
Geraint Palmer <palmer.geraint@googlemail.com>
2018-01-11 09:20:08 +0000

    barod ar gyfer v1.1.5

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ author = u'Geraint Palmer'
 # built documents.
 #
 # The short X.Y version.
-version = '1.1.4'
+version = '1.1.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
