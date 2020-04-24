20dde5d48ed49bf8e97cfe8e26f0a87e12203d5d
Geraint Palmer <palmer.geraint@googlemail.com>
2020-04-24 09:31:45 +0100

    diweddaru rhif fersiwn

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ author = u'Geraint Palmer'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0.0'
+version = '2.1.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
